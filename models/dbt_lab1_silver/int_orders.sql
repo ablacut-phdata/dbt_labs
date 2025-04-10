999f309c0f8bffe4514fef164f2f23a2cac9f3be
Alejandro Blacut <ablacut@phdata.io>
2025-04-10 12:43:15 +0000

    int orders

--- models/dbt_lab1_silver/int_orders.sql
+++ models/dbt_lab1_silver/int_orders.sql
@@ -1,37 +1,47 @@
 {{ config(materialized='view') }}
 
-with bronze_orders as (
-
+with bronze_order_items as (
     SELECT 
-        {{ dbt_utils.generate_surrogate_key(['OrderID']) }} as pk_order_key,
+        {{ dbt_utils.generate_surrogate_key(['OrderID','ProductID']) }} as pk_order_key,
         OrderID as nk_order_id,
-        CustomerID,
-        EmployeeID,
-        OrderDate,
-        RequiredDate,
-        ShippedDate,
-        ShipVia,
-        Freight,
-        ShipName,
-        ShipAddress,
-        ShipCity,
-        ShipRegion,
-        ShipPostalCode,
-        ShipCountry,
-        OrderID,
-        ProductID,
-        UnitPrice,
-        Quantity,
-        Discount
-    FROM {{ var("source_schema") }}
-
+        {{ dbt_utils.generate_surrogate_key(['CustomerID']) }} as fk_customer_key,
+        CustomerID as nk_customer_id,
+        {{ dbt_utils.generate_surrogate_key(['EmployeeID']) }} as fk_employee_key,
+        EmployeeID as nk_employee_id
+        OrderDate as order_date,
+        RequiredDate as required_date,
+        ShippedDate as shipped_date,
+        ShipVia as ship_via,
+        Freight as freight,
+        -- ShipName as ship_name,
+        -- ShipAddress as ship_address,
+        -- ShipCity as ship_city,
+        -- ShipRegion as ship_region,
+        -- ShipPostalCode as ship_postal_code,
+        -- ShipCountry as ship_country,
+        OrderID2 as pnk_order_id,
+        ProductID as product_id,
+        UnitPrice as unit_price,
+        Quantity as quantity,
+        Discount as discount
+    FROM {{ var("source_schema") }}.orders
+    WHERE OrderID IS NOT NULL
 )
 
-select *
-from bronze_orders
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+SELECT 
+    pk_order_key,
+    OrderID as nk_order_id,
+    fk_customer_key,
+    CustomerID as nk_customer_id,
+    fk_employee_key,
+    EmployeeID as nk_employee_id
+    OrderDate as order_date,
+    RequiredDate as required_date,
+    ShippedDate as shipped_date,
+    ShipVia as ship_via,
+    Freight as freight,
+    ProductID as product_id,
+    UnitPrice as unit_price,
+    Quantity as quantity,
+    Discount as discount
+FROM bronze_order_items
